38380116a094b2c97b38898264600542f1455b8f
dino16m <anselem16m@gmail.com>
2020-10-27 15:11:27 +0100

    modified file access

--- WEB/functions/index.js
+++ WEB/functions/index.js
@@ -4,13 +4,16 @@ const engines = require('consolidate');
 // const firebase = 
 const app = express();
 app.get('/login', (request, response) => {
-    response.redirect('/login.html')
+    response.sendFile('/minisidebar/pages-login.html.htm')
+})
+app.get('/register', (request, response) => {
+    response.sendFile('/minisidebar/pages-register.html.htm')
 })
 app.get('/', (request, response) => {
-    response.redirect('/index.html')
+    response.sendFile('/minisidebar/index.html')
 })
 app.get('/dashboard', (request, response) => {
-    response.redirect('/minisidebar/index3.html')
+    response.sendFile('/minisidebar/index3.html')
 })
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
