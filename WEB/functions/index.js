c3a131592d59e034d827b4a0b673eec0bae5a4df
Dino16m <37278830+Dino16m@users.noreply.github.com>
2020-10-27 16:26:46 +0100

    Updated index.js, changed send file to redirect

--- WEB/functions/index.js
+++ WEB/functions/index.js
@@ -4,16 +4,16 @@ const engines = require('consolidate');
 // const firebase = 
 const app = express();
 app.get('/login', (request, response) => {
-    response.sendFile('/minisidebar/pages-login.html.htm')
+    response.redirect('/minisidebar/pages-login.html.htm')
 })
 app.get('/register', (request, response) => {
-    response.sendFile('/minisidebar/pages-register.html.htm')
+    response.redirect('/minisidebar/pages-register.html.htm')
 })
 app.get('/', (request, response) => {
-    response.sendFile('/minisidebar/index.html')
+    response.redirect('/minisidebar/index.html')
 })
 app.get('/dashboard', (request, response) => {
-    response.sendFile('/minisidebar/index3.html')
+    response.redirect('/minisidebar/index3.html')
 })
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
